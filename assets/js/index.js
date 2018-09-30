01ea116f87cd9517e7b1e9e567c3f32a26b91a95
Lynachen <lynachen@126.com>
2018-09-30 11:05:22 +0800

    rem自适应及优化动画效果

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,33 @@
-        //随机背景图片
-        $(function() {
-            var length = 3;
-            $(".bg-img li:nth-child(2)").show();
-            setInterval(function () {
-                var randomBgIndex = Math.round(Math.random() * length);
-                $("#section1 .bg-img li").eq(randomBgIndex).addClass("show").siblings().removeClass("show");
-            },5000);
-        
-            //导航slideToggle
-            $(".more-nav").on("click", function () {
-                $(".nav-ul.nav-xs-ul").slideToggle(300);
-            });
+$(function(){
+    window['adaptive'].desinWidth = 750;
+    window['adaptive'].baseFont = 12;
+    window['adaptive'].init();
+})
+//随机背景图片
+$(function() {
+    var length = 3;
+    $(".bg-img li:nth-child(2)").show();
+    setInterval(function () {
+        var randomBgIndex = Math.round(Math.random() * length);
+        $("#section1 .bg-img li").eq(randomBgIndex).addClass("show").siblings().removeClass("show");
+    },5000);
+    
+    //导航slideToggle
+    $(".more-nav").on("click", function () {
+        $(".nav-ul.nav-xs-ul").slideToggle(300);
+    });
 
-            $(".nav-xs-ul li").click(function () {
-                $(this).addClass("active").siblings().removeClass("active");
-                $(this).parents(".nav-xs-ul").slideUp(300);
-            });
-        });
-        $(window).resize(function (){
-            var wid = $(window).width();
-            if(wid>768){
-                $(".nav-xs-ul").hide();
-            }
-        });
+    $(".nav-xs-ul li").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        $(this).parents(".nav-xs-ul").slideUp(300);
+    });
+});
+$(window).resize(function (){
+    var wid = $(window).width();
+    if(wid>768){
+        $(".nav-xs-ul").hide();
+    }
+});
 
         // function IsPC() {
         //     var userAgentInfo = navigator.userAgent;
